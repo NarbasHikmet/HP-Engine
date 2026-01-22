3df95f9ba9f84f52d78961f9339388e150eab085
NarbasHikmet <108670827+NarbasHikmet@users.noreply.github.com>
2026-01-22 18:09:31 +0300

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -3,8 +3,8 @@ from engine.master_orchestrator import MasterOrchestrator
 events = [
     {"team": "opponent", "type": "pass", "x": 40},
     {"team": "opponent", "type": "pass", "x": 50},
-    {"team": "team", "type": "tackle", "x": 55},
-    {"team": "team", "type": "interception", "x": 60},
+    {"team": "team", "type": "tackle", "x": 70},
+    {"team": "team", "type": "interception", "x": 72},
     {"team": "team", "type": "pass", "x": 75},
     {"team": "opponent", "type": "pass", "x": 80},
 ]
